6bcf9f4290fad736612e7e4961c288bb220ec216
Mathias Moreira <viejocadillac@gmail.com>
2020-08-07 01:33:29 -0300

    Se maqueta el login de usuario y se agrega la autentificacion con google

--- js/login.js
+++ js/login.js
@@ -1,6 +1,32 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+   
+
 
 });
+
+//TODO
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+
+    window.location = 'http://ed3269eccdd3.ngrok.io/'
+    var profile = googleUser.getBasicProfile();
+    console.log(googleUser)
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
+
+
