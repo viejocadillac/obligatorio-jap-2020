a16de80c90da0e06b5a6239f52bafa188ea4b81c
Mathias Moreira <viejocadillac@gmail.com>
2020-08-07 14:49:38 -0300

    Agrega boton de logout

--- js/init.js
+++ js/init.js
@@ -7,6 +7,19 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAoWh9ESaf4ECr9YQ5006lHzOOMya11sF0",
+  authDomain: "obligatorio-jap-2020.firebaseapp.com",
+  databaseURL: "https://obligatorio-jap-2020.firebaseio.com",
+  projectId: "obligatorio-jap-2020",
+  storageBucket: "obligatorio-jap-2020.appspot.com",
+  messagingSenderId: "210642457292",
+  appId: "1:210642457292:web:188b2a0316d84304b8a869"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
@@ -44,15 +57,30 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+
+
   const mensaejePortada = document.getElementById('mensaje-bienvenida')
+  const navLinksContainer = document.getElementById('nav-links-container')
+
+  navLinksContainer.addEventListener('click', logout)
 
   const user = sessionStorage.getItem('user');
   if(user){
     const userObj = JSON.parse(user)
     mensaejePortada.innerHTML += ` ${ userObj.user ? userObj.user : user.getName()}!`
+    navLinksContainer.innerHTML += '<a class="py-2 d-none d-md-inline-block" click="logout()">Cerrar Sesion</a>'
 
   }else {
     window.location = '/obligatorio-jap-2020/login'
   }
 
 });
+
+function logout() {
+  sessionStorage.removeItem('user')
+  firebase.auth().signOut().then(function() {
+    console.log('// Sign-out successful.')
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
