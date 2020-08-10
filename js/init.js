93e1a20f049b052ac5702e81ff881abda3213842
Mathias Moreira <viejocadillac@gmail.com>
2020-08-10 13:05:07 -0300

    Se implementa la pagina de productos. Listando los items contenidos en el json. Se corrige el nombre de las imagenes que estaban intercambiadas (Peugeot 208 y Suzuki Alto)

--- js/init.js
+++ js/init.js
@@ -21,6 +21,24 @@ let firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if ( user ) {
+  // User is signed in.
+  document.getElementById('opciones-usuario').innerHTML = `<option value="user" selected disabled>${user.displayName}</option>`
+  document.getElementById('opciones-usuario').innerHTML += '<option name="logout" value="logout" >Cerrar Sesion</option>'
+  } else {
+    document.getElementById('opciones-usuario').innerHTML = '<option value="user" selected disabled>Anonimo</option>'
+    document.getElementById('opciones-usuario').innerHTML += '<option name="signin" value="signin" disabled>Crearme una cuenta</option>'
+
+  }
+}, function(error) {
+  console.log(error);
+});
+
+
+
+
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
