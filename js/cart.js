99b58fa03069414be09688b1674777a6d5394eda
Mathias Moreira <viejocadillac@gmail.com>
2020-10-02 13:55:18 -0300

    Se implementa el cambio de cantidad de cada producto en el carrito, calcalculando el total

--- js/cart.js
+++ js/cart.js
@@ -1,26 +1,84 @@
 /* global getJSONData CART_INFO_URL renderIn */
 
-const generateCartItemHTML = (item) => `
-    <article class="cart-item">
-    <img src="${item.src}" alt="${item.name}" class="cart-item__img">
-    <div>
-      <p class="cart-item__name">${item.name}</p>
-      <p class="cart-item__price">${item.currency} ${item.unitCost}</p>
-    </div>
+/**
+ * Funcion que retorna un array con los subtotales de cada item del carrito
+ * @param { HTMLElement } cartElement Contenedor donde se renderiza cada elemento del carrito
+ * @returns {[Number]} Array de subtotales
+ */
+const getAllSubtotals = (cartElement) => {
+  const itemsCollection = cartElement.getElementsByTagName('tr');
 
-    <div class="cart-item__countcontrols">
-      <i class="fa fa-plus"></i>
-      <p class="cart-item__count">${item.count}</p>
-      <i class="fa fa-minus"></i>
-    </div>          
-  </article>
-`;
+  // Se convierte la coleccion a array para tener disponible el metodo map.
+  const items = Array.from(itemsCollection);
+
+  // Se extrae del dataset de cada tr que tenga como atributo name=subtotal, la propiedad subtotal,
+  // generando un array con todos los subtotales.
+  const subtotals = items.map((tr) => tr.querySelector("[name='subtotal']").dataset.subtotal);
+  return subtotals;
+};
+
+const calculateSubtotal = (subtotals) => {
+  const sum = (acc, value) => acc + parseInt(value, 10);
+  return subtotals.reduce(sum, 0);
+};
+
+/**
+ * Funcion que calcula el precio de envio
+ * TODO Agregar la logica del calculo dependiendo de la opcion seleccionada.
+ * @returns {number} Precio de envio
+ */
+const getDeliveryCost = () => 0;
+
+
+
+const updateTotal = () => {
+  const cartElement = document.getElementById('items-container');
+  const subtotals = getAllSubtotals(cartElement);
+  const subtotal = calculateSubtotal(subtotals);
+
+  document.getElementById('subtotal').innerHTML = subtotal;
+  document.getElementById('total').innerHTML = `UYU ${subtotal + getDeliveryCost()};`;
+};
+
+// eslint-disable-next-line no-unused-vars
+const onCountChange = (e) => {
+  const newProductCount = e.querySelector('td input').value;
+
+  const unitCostElement = e.querySelector('td[name="unit-cost"]');
+  const subtotalElement = e.querySelector('td[name="subtotal"]');
+
+  // Se separa el string, por un lado la unidad monetaria y por el otro costo.
+  const [currency, unitCostString] = unitCostElement.innerText.split(' ');
+
+  const unitCost = parseInt(unitCostString, 10);
+  subtotalElement.innerHTML = `${currency} ${unitCost * newProductCount}`;
+  subtotalElement.setAttribute('data-subtotal', unitCost * newProductCount);
+  updateTotal();
+};
+
+const generateCartItemHTML = (product) => {
+  const subTotal = product.count * product.unitCost;
+  return `
+    <tr oninput="onCountChange(this)">
+      <td>
+          <img class="cart-item__img" src="${product.src}"></img>
+      </td>
+      <td>${product.name}</td>
+      <td name="unit-cost">${product.currency} ${product.unitCost}</td>
+      <td>
+          <input type="number" value="${product.count}" min=1>
+      </td>
+      <td name="subtotal" data-subtotal=${subTotal} data-currency=${product.currency}>
+        ${product.currency} ${subTotal}
+      </td>
+    </tr>
+  `;
+};
 
 // Función que se ejecuta una vez que se haya lanzado el evento de
 // que el documento se encuentra cargado, es decir, se encuentran todos los
 // elementos HTML presentes.
 document.addEventListener('DOMContentLoaded', () => {
-
   getJSONData(CART_INFO_URL, false).then(({ data }) => {
     const itemsContainer = document.getElementById('items-container');
 
@@ -28,9 +86,6 @@ document.addEventListener('DOMContentLoaded', () => {
     data.articles.map(generateCartItemHTML).forEach(renderIn(itemsContainer));
     data.articles.map(generateCartItemHTML).forEach(renderIn(itemsContainer));
     data.articles.map(generateCartItemHTML).forEach(renderIn(itemsContainer));
-
-
-  
   });
 
   const toggleFooter = document.getElementById('btn-toggle-checkout');
