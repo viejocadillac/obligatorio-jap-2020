e618107bd1d3f8d167c368811623ed7e9c6083f8
Mathias Moreira <viejocadillac@gmail.com>
2020-10-06 09:24:19 -0300

    Se reimplementa el carrito

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,7 @@
 /**
  * Funcion que retorna un array con los subtotales de cada item del carrito
  * @param { HTMLElement } cartElement Contenedor donde se renderiza cada elemento del carrito
- * @returns {[Number]} Array de subtotales
+ * @returns {[Object]} Array de subtotales
  */
 const getAllSubtotals = (cartElement) => {
   const itemsCollection = cartElement.getElementsByTagName('tr');
@@ -13,8 +13,10 @@ const getAllSubtotals = (cartElement) => {
 
   // Se extrae del dataset la propiedad subtotal,
   // generando un array con todos los subtotales.
-  const subtotals = items.map((tr) => (
-    { currency: tr.dataset.currency, cost: parseInt(tr.dataset.subtotal, 10) }));
+  const subtotals = items.map((tr) => ({
+    currency: tr.dataset.currency,
+    cost: parseInt(tr.dataset.subtotal, 10),
+  }));
   return subtotals;
 };
 
@@ -26,14 +28,11 @@ const CURRENCIES = {
 
 const convertToPeso = (value) => value.cost * CURRENCIES[value.currency];
 
-/**
- * Calcula la suma de todos los productos en el array pasado
- */
-const calculateSubtotal = (subtotals) => subtotals.reduce((acc, value) => acc + value, 0);
+const sumAllValues = (subtotals) => subtotals.reduce((acc, value) => acc + value, 0);
 
 /**
  * Funcion que calcula el precio de envio
- * TODO Agregar la logica del calculo dependiendo de la opcion seleccionada.
+ * @param {number} subtotal Subtotal sobre el que se calcula el precio de envio
  * @returns {number} Precio de envio
  */
 const getDeliveryCost = (subtotal) => {
@@ -42,110 +41,124 @@ const getDeliveryCost = (subtotal) => {
   return Math.round(percentDelivery * subtotal);
 };
 
-
-const getFormatedCost = (prefix, number) => {
+/**
+ * @param {number} number Cantidad a formatear
+ * @returns {string} Cadena formateada
+ */
+const getFormatedNumberWithLocale = (number) => {
+  // TODO Detectar Locale
   const numberFormater = new Intl.NumberFormat('es-UY');
-  return `${prefix} ${numberFormater.format(number)}`;
+  return numberFormater.format(number);
 };
 
 /**
- * Convierte el valor pasado en pesos a la moneda indicada
+ * Convierte el valor pasado en pesos a la moneda seleccionada
+ * La moneda indicada debe ser parte del array CURRENCIES.
  * @param {number} value Valor a convertir
- * @param {string} currency Moneda a la cual se quiere convertir
  */
-const convertPesoTo = (value, currency) => value / CURRENCIES[currency];
+const convertPesoToSelectedCurrency = (value) => {
+  const selectedCurrency = document.getElementById('select-currency').value;
+  return value / CURRENCIES[selectedCurrency];
+};
+
+const formatMoney = (ammount) => {
+  const selectedCurrency = document.getElementById('select-currency').value;
+  return `${selectedCurrency} ${getFormatedNumberWithLocale(ammount)}`;
+};
 
 const updateTotal = () => {
   const cartElement = document.getElementById('items-container');
   const subtotals = getAllSubtotals(cartElement);
 
   const subtotalsInPesos = subtotals.map(convertToPeso);
-  const subtotalInPesos = calculateSubtotal(subtotalsInPesos);
+  const subtotalInPesos = sumAllValues(subtotalsInPesos);
 
-  const selectedCurrency = document.getElementById('select-currency').value;
+  const subtotal = convertPesoToSelectedCurrency(subtotalInPesos);
+  const deliveryCost = getDeliveryCost(subtotal);
+  const totalCost = subtotal + deliveryCost;
 
-  const subtotalConverted = convertPesoTo(subtotalInPesos, selectedCurrency);
-
-  const deliveryCost = getDeliveryCost(subtotalConverted);
-  const totalCost = subtotalConverted + deliveryCost;
-
-  document.getElementById('subtotal').innerHTML = getFormatedCost(selectedCurrency, subtotalConverted);
-  document.getElementById('envio').innerHTML = getFormatedCost(selectedCurrency, deliveryCost);
-  document.getElementById('total').innerHTML = getFormatedCost(selectedCurrency, totalCost);
+  document.getElementById('subtotal').innerHTML = formatMoney(subtotal);
+  document.getElementById('envio').innerHTML = formatMoney(deliveryCost);
+  document.getElementById('total').innerHTML = formatMoney(totalCost);
 };
 
 // eslint-disable-next-line no-unused-vars
-const onCountChange = (e) => {
-  const { currency, unitcost } = e.dataset;
+const onCountChange = (cartItem) => {
+  const { unitcost } = cartItem.dataset;
   const unitCostInt = parseInt(unitcost, 10);
 
-  const newProductCount = e.querySelector('td input').value;
-  const subtotalElement = e.querySelector('td[name="subtotal"]');
+  const newProductCount = cartItem.querySelector('td input').value;
+  const subtotalElement = cartItem.querySelector('td[name="subtotal"]');
 
   const subtotal = unitCostInt * newProductCount;
 
-  subtotalElement.innerHTML = getFormatedCost(currency, subtotal);
-  e.setAttribute('data-subtotal', unitCostInt * newProductCount);
+  subtotalElement.innerHTML = formatMoney(subtotal);
+
+  cartItem.setAttribute('data-subtotal', unitCostInt * newProductCount);
   updateTotal();
 };
 
-const generateCartItemHTML = (product) => {
-  const subtotal = product.count * product.unitCost;
+const generateCartItemHTML = ({
+  currency,
+  unitCost,
+  name,
+  count,
+  src,
+}) => {
+  const subtotal = count * unitCost;
   return `
     <tr
         class="cart-item"
         oninput="onCountChange(this)"
-        data-currency=${product.currency}
-        data-unitcost=${product.unitCost}
+        data-currency=${currency}
+        data-unitcost=${unitCost}
         data-subtotal=${subtotal}
       >
-      <td class="cart-item__celda">
-          <img class="cart-item__img" src="${product.src}"></img>
+      <td class="cart-item__celda cart-item__celda-img">
+          <img src="${src}" class="cart-item__img"></img>
       </td>
-      <td class="cart-item__celda cart-item__name">${product.name}</td>
-      <td class="cart-item__celda" name="unit-cost">${getFormatedCost(product.currency, product.unitCost)}</td>
-      <td class="cart-item__celda">
-          <input type="number" class="cart-item__count" value="${product.count}" min=1>
+      <td class="cart-item__name" >${name}</td>
+      <td class="cart-item__cost" name="unit-cost" data-th="Precio">${formatMoney(unitCost)}</td>
+      <td class="cart-item__count" data-th="x">
+          <input type="number" class="cart-item__count-input" value="${count}" min=1>
       </td>
-      <td name="subtotal" class="cart-item__celda cart-stats__number">
-      ${getFormatedCost(product.currency, subtotal)}
+      <td name="subtotal" class="cart-item__subtotal">
+      ${formatMoney(subtotal)}
       </td>
     </tr>
   `;
 };
 
+const generateCurrencyOptionHTML = (currency) => `<option value="${currency}">${currency}</option>`;
+
 // Función que se ejecuta una vez que se haya lanzado el evento de
 // que el documento se encuentra cargado, es decir, se encuentran todos los
 // elementos HTML presentes.
 document.addEventListener('DOMContentLoaded', () => {
-  getJSONData(CART_INFO_URL, false).then(({ data }) => {
+  getJSONData(CART_INFO_URL, false).then(({
+    data,
+  }) => {
     const itemsContainer = document.getElementById('items-container');
 
     data.articles.map(generateCartItemHTML).forEach(renderIn(itemsContainer));
-    // data.articles.map(generateCartItemHTML).forEach(renderIn(itemsContainer));
-    // data.articles.map(generateCartItemHTML).forEach(renderIn(itemsContainer));
-    // data.articles.map(generateCartItemHTML).forEach(renderIn(itemsContainer));
+
+    
 
     updateTotal();
   });
 
   // Seleccion del metodo de envio
-  const selectEnvio = document.getElementById('select-envio');
-  selectEnvio.addEventListener('change', () => {
+  document.getElementById('select-envio').addEventListener('change', () => {
     updateTotal();
   });
 
-  // Se completan las monedas en el select
   const selectCurrency = document.getElementById('select-currency');
-  Object.keys(CURRENCIES).forEach((currency) => {
-    selectCurrency.innerHTML += `<option value="${currency}">${currency}</option>`;
-  });
+  // Se completan las monedas en el select
+  Object.keys(CURRENCIES)
+    .map(generateCurrencyOptionHTML)
+    .forEach(renderIn(selectCurrency));
 
   selectCurrency.addEventListener('change', () => {
-    updateTotal();
-  });
-
-  selectEnvio.addEventListener('change', () => {
     updateTotal();
   });
 
@@ -153,7 +166,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let footerIsShown = false;
   toggleFooter.addEventListener('click', () => {
     const icon = document.querySelector('.icon-toggle-footer');
-    const cartFooter = document.querySelector('.cart__footer');
+    const cartFooter = document.querySelector('.cart-checkout');
     if (footerIsShown) {
       icon.style.transform = 'rotate(0deg)';
       cartFooter.style.maxHeight = '3rem';
