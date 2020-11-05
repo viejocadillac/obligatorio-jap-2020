f8bb823bcc485c9f32c7dffc69336a362ee016cf
Mathias Moreira <viejocadillac@gmail.com>
2020-11-05 01:55:23 -0300

    Se elimina variable innecesaria

--- js/register.js
+++ js/register.js
@@ -11,10 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(userInfo)
     firebase.auth().createUserWithEmailAndPassword(userInfo.email, userInfo.password).catch(function(error) {
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage)
-      // ...
+      const errorCode = error.code;
+      console.log(errorCode);
     });
   });
 });
