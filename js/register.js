84413d74679fb1d9dd23156ddf26ebbd1db3f00d
Mathias Moreira <viejocadillac@gmail.com>
2020-11-08 14:16:03 -0300

    Se agrega manejo de errores y de estado del boton de registro

--- js/register.js
+++ js/register.js
@@ -1,4 +1,14 @@
-/* global firebase, redirectTo, PROFILE, getFormDataObject hideSpinner */
+/* global
+  firebase,
+  redirectTo,
+  PROFILE,
+  getFormDataObject,
+  hideSpinner,
+  createStateButtonHandler,
+  validateAll,
+  renderIn,
+  getFormErrorText
+*/
 const saveUserInDB = (user, userExtraData) => {
   const userRef = firebase.database().ref(`users/${user.uid}`);
   return userRef.set(userExtraData);
@@ -8,24 +18,39 @@ document.addEventListener('DOMContentLoaded', () => {
   hideSpinner();
   const btnRegister = document.getElementById('btn-register');
   const userInfoForm = document.getElementById('personal-info');
+  const btnSendFormIcon = document.getElementById('btn-send-form-icon');
+  const errorContainer = document.getElementById('register-error');
+
+  // Se crea el manejador de estado del boton de registro
+  const buttonStateHandler = createStateButtonHandler(
+    btnRegister,
+    btnSendFormIcon,
+    'fa-spinner',
+    'fa-sign-in-alt',
+  );
+
   btnRegister.addEventListener('click', () => {
-    const userInfo = getFormDataObject(userInfoForm);
-    const { email, password, displayName } = userInfo;
+    if (validateAll(userInfoForm)) {
+      buttonStateHandler.start();
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).then((response) => {
-      const { user } = response;
-      const initData = {
-        email,
-        displayName,
-        photoURL: '',
-        phoneNumber: '',
-        emailVerified: false,
-      };
+      const userInfo = getFormDataObject(userInfoForm);
+      const { email, password, displayName } = userInfo;
 
-      saveUserInDB(user, initData).then(() => redirectTo(PROFILE)).catch((err) => {console.log(error)});
-    }).catch((error) => {
-      // TODO Manejar errores
-      console.log(error)
-    });
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((response) => {
+          const { user } = response;
+          const initData = {
+            email,
+            displayName,
+            photoURL: '',
+            phoneNumber: '',
+            emailVerified: false,
+          };
+
+          saveUserInDB(user, initData).then(() => redirectTo(PROFILE));
+        })
+        .catch((error) => renderIn(errorContainer)(getFormErrorText(error.code)))
+        .finally(() => buttonStateHandler.stop());
+    }
   });
 });
